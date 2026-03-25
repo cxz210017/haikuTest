7628931b0e7e7e3d431ec6b6cff4a896feeeae2b
xtine burrough <xtineburrough@gmail.com>
2026-03-25 20:05:49 +0000

    formatted

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,22 @@
 // main.js
+//Define Random Number Function
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min) + 1) + min;
+}
 
-console.log("Hello, World!");
+//Define divs variable as all divs inside grid container
+let divs = document.querySelectorAll(".grid-container div");
+
+//Define Randomize function
+function randomize() {
+  divs.forEach(function (div) {
+    let scale = randomNumber(0.5, 1.2);
+    let translateX = randomNumber(0, 50);
+    let translateY = randomNumber(0, 20);
+    let rotate = randomNumber(0, 360);
+    div.style.transform = `scale(${scale}) translate(${translateX}%, ${translateY}%) rotate(${rotate}deg)`;
+  }
+);
+  console.log(divs.length, "randomize!");
+}
+document.addEventListener("click", randomize);
